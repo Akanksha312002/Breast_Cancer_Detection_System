2504a581283568e7dcd4ec9b3d36180ac1b8c8be
Akanksha Ghorpade <ghorpadeakanksha31@gmail.com>
2026-03-06 23:37:01 +0530

    fix render port binding

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
+import os
 
 from routes.prediction_routes import prediction
 from routes.auth_routes import auth
@@ -10,7 +11,8 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["JWT_SECRET_KEY"] = "change_this_to_a_strong_secret_key_123"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=7)  # or hours=6
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=7)
+
 jwt = JWTManager(app)
 
 app.register_blueprint(prediction)
@@ -21,4 +23,5 @@ def home():
     return "Server is running successfully 🚀"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
